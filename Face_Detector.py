e45af10b29cef23bae4115dc00818375772e8575
Chen-Kuan Liao <czl5899@psu.edu>
2022-06-09 09:28:14 -0400

    finish image detection

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,47 +1,62 @@
-import sys
 import cv2
 from random import randrange
-from os import system, name
-import time
-import keyboard
-import threading
+from os import system, name, listdir
+from os.path import isfile, join
 
 
 def Run_Image_Face_Detector():
-    print('Press any key to exit')
-
     # loading some pre-trained data on face frontals from opencv (haar cascade algorithm)
     trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    # choose an image to detect faces in
-    img = cv2.imread('images/' + '2friends.png')
+    onlyfiles = [f for f in listdir('images/') if isfile(join('images/', f))]
 
-    # converting the image to grayscale => for the sake of using haar cascade algorithm
-    grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    while True:
+        print('------------------------------------------------')
+        for file in onlyfiles:
+            print(f"{onlyfiles.index(file)} {file}")
+        print('------------------------------------------------\n')
 
-    # detect faces
-    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+        print('Please enter a number that corresponds to the image you want to detect')
+        print('(Enter q or Q to exit)')
 
-    # draw rectangles around the faces
-    for coordinates in face_coordinates:
-        # getting the coordinates
-        (x, y, w, h) = coordinates
-        cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(128, 256), randrange(128, 256), randrange(128, 256)), 3)
+        value = input()
 
-    # showing the image
-    cv2.imshow('Image Face Detector', img)
+        if value == 'q' or value == 'Q':
+            print('Leaving Image Face Detection Mode ...')
+            return
 
-    # wait until a key is pressed
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        if value.isnumeric() and int(value) < len(onlyfiles) and int(value) >= 0:
+            print(f"You have selected the image, {onlyfiles[int(value)]}, to be detected ...")
 
-    print('Leaving Image Face Detection Mode ...')
-    return
+            # choose an image to detect faces in
+            img = cv2.imread('images/' + onlyfiles[int(value)])
 
+            # converting the image to grayscale => for the sake of using haar cascade algorithm
+            grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # detect faces
+            face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+            # draw rectangles around the faces
+            for coordinates in face_coordinates:
+                # getting the coordinates
+                (x, y, w, h) = coordinates
+                cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(128, 256), randrange(128, 256), randrange(128, 256)), 3)
+
+            # showing the image
+            cv2.imshow('Image Face Detector', img)
+
+            # wait until a key is pressed
+            print('Press any key to close image')
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            
+        else:
+            print('\n!! Invalid Input !!\n')
+    
 def Run_WebCam_Face_Detector():
     # loading some pre-trained data on face frontals from opencv (haar cascade algorithm)
     trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
 
     print('Opening your default WebCam ...')
 
@@ -77,19 +92,17 @@ def Run_WebCam_Face_Detector():
         if key != -1:
             cv2.destroyAllWindows()
             print('Leaving WebCam Face Detection Mode ...')
+            # release the videoCapture object
+            webcam.release()
             break
 
     return
-
-
-    # release the videoCapture object
-    webcam.release()
 
 if name == 'nt':
     _ = system('cls')
 
 while True:
-    print('Please enter:')
+    print('\nPlease enter:')
     print('       0 to exit')
     print('       1 for Image Face Detection Mode')
     print('       2 for WebCam Face Detection Mode')
@@ -109,9 +122,6 @@ while True:
         print('Entered WebCam Face Detection Mode ...')
         Run_WebCam_Face_Detector()
         print()
-        
     
     else:
         print('\n!!Please enter a valid input!!\n')
-
-
